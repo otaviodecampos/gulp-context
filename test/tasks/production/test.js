eab12b614f912bc71c56fd489ec384d3ea4a7590
otaviodecampos <otaviodecampos@hotmail.com>
2015-08-04 22:45:13 -0300

    Add from-context-target feature

--- test/tasks/production/test.js
+++ test/tasks/production/test.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
-var htmlify = require('gulp-angular-htmlify');
+var minifyHtml = require('gulp-minify-html');
 
 function copy() {
 
     var input = [this.sourceDir + '**/*.html'];
 
     return gulp.src(this.watch(input))
-        .pipe(htmlify())
+        .pipe(minifyHtml())
         .pipe(gulp.dest(this.targetDir));
 }
 
