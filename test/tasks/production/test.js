5a8afcc24117132d8426a14b298af7c48655c929
otaviodecampos <otaviodecampos@hotmail.com>
2015-08-06 01:11:22 -0300

    Add input function to task scope

--- test/tasks/production/test.js
+++ test/tasks/production/test.js
@@ -3,9 +3,9 @@ var minifyHtml = require('gulp-minify-html');
 
 function copy() {
 
-    var input = [this.sourceDir + '**/*.html'];
+    var input = this.input(this.sourceDir, ['**/*.html']);
 
-    return gulp.src(this.watch(input))
+    return gulp.src(input)
         .pipe(minifyHtml())
         .pipe(gulp.dest(this.targetDir));
 }
